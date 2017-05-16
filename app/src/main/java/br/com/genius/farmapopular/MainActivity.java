973c492648bb2d2cb80a0e6b6ddb5d40bf4f9fef
Luiz Brito <luizhsbrito@gmail.com>
2017-05-16 00:18:03 -0300

    Criaação da função Discar 136.

--- app/src/main/java/br/com/genius/farmapopular/MainActivity.java
+++ app/src/main/java/br/com/genius/farmapopular/MainActivity.java
@@ -1,14 +1,36 @@
 package br.com.genius.farmapopular;
-
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClickDiscar(View v) {
+
+        String numero ="tel:"  + 136;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_CALL,Uri.parse(numero)));
+
+
     }
 }
