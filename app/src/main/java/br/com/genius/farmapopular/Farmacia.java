314883466802aa9cef74d323310928353546b3d2
Clebson Ferreira <cclebsonfdasilva@gmail.com>
2017-05-16 09:47:15 -0300

    Revert "Commit Lista Farmacia"
    This reverts commit b4264e82ec762f8aeac469d49ce3e7fa99007f5f.

--- app/src/main/java/br/com/genius/farmapopular/Farmacia.java
+++ app/src/main/java/br/com/genius/farmapopular/Farmacia.java
@@ -1,6 +1,9 @@
 package br.com.genius.farmapopular;
 
 import android.app.ListActivity;
+import android.net.Uri;
+import android.opengl.EGLExt;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ArrayAdapter;
@@ -12,7 +15,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Delayed;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -23,8 +29,8 @@ import static java.lang.Thread.sleep;
  */
 
 public class Farmacia extends ListActivity {
-    ArrayList<Dados> Lista=new ArrayList<>();
-
+    List Lista;
+    String[] items = new String[]{"Clebson"};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,27 +41,18 @@ public class Farmacia extends ListActivity {
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
                 try {
-                    ArrayList<String> ListaLocal=new ArrayList<>();
                     JSONArray jsonArray = response.getJSONArray("resultset");
                     for (int i = 0; i < jsonArray.length(); i++) {
-                      Dados d=new Dados();
-                        d.setDados(jsonArray.getJSONArray(i).toString());
-                        Lista.add(d);
+                        Lista.add(jsonArray.getJSONObject(i));
                     }
-                    execulte();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
-    }
-    public void execulte(){
-        ArrayList<String> Listatela=new ArrayList<>();
-        for (int j=0;j<Lista.size();j++){
-            Listatela.add(Lista.get(j).getDados().toString());
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Listatela);
-        setListAdapter(adapter);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Lista);
+            setListAdapter(adapter);
+
     }
 
 }
