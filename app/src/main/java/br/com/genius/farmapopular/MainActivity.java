5f5c56b887bccd81c5c250a180cad6f22711bc08
luiz.brito <Sk8pqrth!@#>
2017-05-20 13:13:23 -0300

    1.Função mapa.(Efetuar os testes no aparelho,na VM não rodou). 2.Alteração nome Botão Localizar  Farmacia.

--- app/src/main/java/br/com/genius/farmapopular/MainActivity.java
+++ app/src/main/java/br/com/genius/farmapopular/MainActivity.java
@@ -1,25 +1,10 @@
 package br.com.genius.farmapopular;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import static android.R.attr.alertDialogIcon;
-import static android.R.attr.permission;
-import static android.R.attr.phoneNumber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(Intent.ACTION_CALL, uri);
         startActivity(intent);
+
+    }
+
+    public void OnClickMapa(View v){
+        Intent z=new Intent(this,MapsActivity.class);
+        startActivity(z);
 
     }
 
