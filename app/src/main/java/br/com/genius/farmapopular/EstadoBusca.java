eceefd38843d44853fb58edfab30f9d3c6b14c44
NosbelcFerreira <cclebsonfdasilva@gmail.com>
2017-05-21 16:22:55 -0300

    commit modificaçõe buscas

--- app/src/main/java/br/com/genius/farmapopular/EstadoBusca.java
+++ app/src/main/java/br/com/genius/farmapopular/EstadoBusca.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by Desenvolvimento on 19/05/2017.
  */
 
-public class EstadoBusca extends Activity {
+public class EstadoBusca extends Activity{
     AutoCompleteTextView auto;
     String[]uf=new String[]{"AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ",
                             "RN","RS","RO","RR","SC","SP","SE","TO"};
@@ -21,17 +22,16 @@ public class EstadoBusca extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView(R.layout.select_estado);
-        auto=(AutoCompleteTextView)findViewById( R.id.autoCompleat);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,uf);
-        auto.setThreshold(1);
-        auto.setAdapter(adapter);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, uf);
+        dropdown.setAdapter(adapter1);
     }
 
     public void Buscar(View v){
         Intent intent=new Intent(EstadoBusca.this,Farmacia.class);
-        EditText editText=(EditText)findViewById( R.id.autoCompleat);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
         String text="";
-        text=editText.getText().toString();
+        text=dropdown.getSelectedItem().toString();
         Bundle bundle=new Bundle(  );
         bundle.putString("text",text);
         intent.putExtras(bundle);
