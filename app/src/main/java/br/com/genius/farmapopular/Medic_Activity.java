5d4fd3a4add9f46641df8298f9e03e14b4a51e75
Josuel <josuelmartins.rodrigues@gmail.com>
2017-05-21 18:02:00 -0300

    Tela adquirir implemetada em desenvolvimento para o texto

--- app/src/main/java/br/com/genius/farmapopular/Medic_Activity.java
+++ app/src/main/java/br/com/genius/farmapopular/Medic_Activity.java
@@ -8,6 +8,6 @@ public class Medic_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medic); //chamando
+        setContentView(R.layout.activity_medic); //
     }
 }
